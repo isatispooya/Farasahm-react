36a57d0f647842f772da70db36e3be59db75adba
m-javad <m.javad7721@gmail.com>
2024-01-27 10:47:29 +0330

    .

--- src/page/subPage/noBours/Priority.js
+++ src/page/subPage/noBours/Priority.js
@@ -23,8 +23,8 @@ const Priority = () =>{
     const [datePriorityLst, setDatePriorityLst] = useState(null)
     const [loading, setLoading] = useState(true)
 
-    // console.log(datePriority)
-    // console.log(datePriorityLst)
+    console.log(datePriority)
+    console.log(datePriorityLst)
 
     const [allName, setAllName] = useState([])
     const [df, setDf] = useState([])
@@ -42,8 +42,11 @@ const Priority = () =>{
         axios.post(OnRun+'/getdatepriority',{access:access})
         .then(response=>{
             if (response.data.reply) {
+                const lst = response.data.lst;
+                const lastDate = lst[lst.length - 1]['date'];
                 setDatePriority(response.data.lst[0]['date'])
-                setDatePriorityLst(response.data.lst)
+                setDatePriority(lastDate);
+                setDatePriorityLst(lst);
             }else{
                 toast.success(response.data.msg,{position: toast.POSITION.BOTTOM_RIGHT})
             }
