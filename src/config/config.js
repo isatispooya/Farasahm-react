844bd78913650b0833c33d2679a51e11483767a4
mohammadali <mohammadalisadat.20@gmail.com>
2024-07-13 14:16:17 +0330

    see settings

--- src/config/config.js
+++ src/config/config.js
@@ -1,1 +1,2 @@
-export const OnRun = 'http://192.168.62.131:8000' /*'https://b.fidip.ir' 'http://127.0.0.1:8000*/
+export const OnRun =
+  "https://b.fidip.ir"; /*'https://b.fidip.ir' 'http://127.0.0.1:8000*/
