d30f0b94eee75af81170cc666d2b531de639ceca
mohammadali <mohammadalisadat.20@gmail.com>
2024-07-30 10:37:37 +0330

    .

--- src/page/marketing.jsx
+++ src/page/marketing.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import SliderComponent from '../componet/saham';   
-import Date from '../componet/date';   
+import React, { useState } from "react";
+import ModalFilter from "../componet/modalFilter";
+
+import SliderComponent from "../componet/saham";
+import Date from "../componet/date";
 
 const Marketing = () => {
+  const [isModalOpen, setIsModalOpen] = useState(true);
 
-    return ( <>
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
 
-        <div >
-            <Date/>
-         <SliderComponent/>
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        {isModalOpen && <ModalFilter toggleModal={toggleModal} />}
+        <div>
+          <Date />
+          <SliderComponent />
         </div>
-    </> );
-}
- 
+      </div>
+    </>
+  );
+};
 export default Marketing;
