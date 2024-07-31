4ed239cf19fa506e4636d92b14c4c9d955ed756a
mohammadali <mohammadalisadat.20@gmail.com>
2024-07-31 12:58:15 +0330

    .

--- src/componet/slider.jsx
+++ src/componet/slider.jsx
@@ -24,7 +24,7 @@ const SliderComponent = () => {
       console.log("Input 2:", num2);
     } else {
       // Handle invalid inputs (e.g., show an error message)
-      alert('لطفا مقدار عددی و مناسب وارد کنید');
+      alert('Please enter valid numbers in both fields.');
     }
   };
 
@@ -44,30 +44,6 @@ const SliderComponent = () => {
           className="w-full p-2 text-center border border-gray-300 rounded"
         />
       </div>
-      <Slider
-        className="w-full h-2 "
-        thumbClassName="h-3 w-3  bg-blue-500 rounded-full  cursor-pointer"
-        value={value}
-        onChange={handleSliderChange}
-        min={0}
-     
-        renderTrack={(props, state) => (
-          <div
-            {...props}
-            className={`${
-              state.index === 0 ? 'bg-blue-500' : 'bg-gray-300'
-            } h-2 rounded-full`}
-            style={{
-              ...props.style,
-              left: state.index === 0 ? '0%' : `${value}%`,
-              width: state.index === 0 ? `${value}%` : `${100 - value}%`,
-            }}
-          />
-        )}
-        renderThumb={(props) => (
-          <div {...props} className="h-3 w-3  z-10 bg-blue-500  rounded-full cursor-pointer" />
-        )}
-      />
 
       {/* Second input */}
       <div className="mb-5">
