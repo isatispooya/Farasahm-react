6517207694aa10160f8ead0d47d22b5a3f0fb78a
somayeh124 <karimzadeh.somayeh1380@gmail.com>
2024-08-01 10:21:59 +0330

    //

--- src/componet/smspage.jsx
+++ src/componet/smspage.jsx
@@ -1,26 +1,85 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef } from "react";
+import axios from "axios";
+import { OnRun } from "../config/config";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
-const Smspage = () => {
-  const [message, setMessage] = useState('');
-  const [preview, setPreview] = useState(''); 
-  const words = ['سلام', 'چطورید؟', 'تاریخ', 'ایساتیس پویا', 'صنایع مفتول', 'فیلتر', 'فراسهم', 'کارگزاری', 'اطلاعات'];
+
+const Smspage = ({ toggleModal, access, columns, Config }) => {
+  const [message, setMessage] = useState("");
+  const [context, setContext] = useState("");
+  const [sendMessage, setSendMessage] = useState("");
   const textareaRef = useRef(null);
-
+  
   const handleWordSelect = (word) => {
-    setMessage((prevMessage) => prevMessage + ' ' + word + ' ');
+    setMessage((prevMessage) => prevMessage + " " + word + " ");
     textareaRef.current.focus();
   };
-
+  
+  const handleClose = () => {
+    toggleModal(false);
+  };
+  
   const handlePreview = () => {
-    setPreview(message);  
+    sendMessageToBackend();
+  };
+  const handleSend = () => {
+    send();
+  };
+
+  const sendMessageToBackend = () => {
+    axios({
+      method: "POST",
+      url: `${OnRun}/marketing/perviewcontext`,
+      data: { access: access, context: message, _id: Config },
+    })
+    .then((response) => {
+        setContext(response.data);
+      })
+      .catch((error) => {
+        console.error("error:", error);
+      });
+  };
+
+
+  const send= () => {
+    axios({
+      method: "POST",
+      url: `${OnRun}/marketing/sendsms`,
+      data: { access: access, context: message, _id: Config },
+    })
+      .then((response) => {
+        setSendMessage(response.data);
+        console.log("esponse",response.data);
+        if (response.data.reply) {
+          toast.success("پیام با موفقیت ارسال شد!", {
+            position: "top-right",
+            autoClose: 3000,
+          });
+        } else {
+          toast.error("ارسال پیام با مشکل مواجه شد.", {
+            position: "top-right",
+            autoClose: 3000,
+          });
+        }
+      })
+      .catch((error) => {
+        console.error("There was an error with the request:", error);
+        toast.error("خطایی رخ داد. لطفاً دوباره تلاش کنید.", {
+          position: "top-right",
+          autoClose: 3000,
+        });
+      });
+      
   };
 
   return (
-    <div dir='rtl' className="flex flex-col  items-center justify-center min-h-screen bg-gray-100 p-4">
-      <div className="flex w-full max-w-4xl space-x-4">
-        <div className="w-1/5 bg-white rounded-lg shadow-lg p-4 overflow-y-auto  max-h-96">
+    <div dir="rtl" className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+      <ToastContainer />
+      <div className="flex w-full max-w-7xl items-center justify-center  space-x-4">
+        <div className="w-1/5 bg-white rounded-lg shadow-lg p-4 overflow-y-auto max-h-96">
           <div className="flex flex-col space-y-2">
-            {words.map((word, index) => (
+            {columns.map((word, index) => (
               <button
                 key={index}
                 onClick={() => handleWordSelect(word)}
@@ -38,25 +97,38 @@ const Smspage = () => {
           onChange={(e) => setMessage(e.target.value)}
           className="w-2/3 h-96 p-4 text-lg border rounded-lg shadow-lg"
           placeholder="پیام خود را بنویسید..."
-          style={{ direction: 'rtl' }}
+          style={{ direction: "rtl" }}
         />
       </div>
+      <div className="flex gap-10">
+        <button
+          onClick={handleSend}
+          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-md self-center mt-4"
+        >
+          ارسال
+        </button>
+        <button
+          onClick={handlePreview}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md self-center mt-4"
+        >
+          پیش نمایش
+        </button>
+        <button
+          onClick={handleClose}
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-md self-center mt-4"
+        >
+          بستن
+        </button>
+      </div>
 
-      <button
-        onClick={handlePreview}
-        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md self-center mt-4'
-      >
-        پیش نمایش
-      </button>
-
-      {preview && (
-        <div className="mt-6 p-4 w-full max-w-4xl bg-white rounded-lg shadow-lg">
-          <h3 className="text-lg  mb-2">پیش‌ نمایش پیام:</h3>
-          <p className="text-gray-700" style={{ whiteSpace: 'pre-wrap', direction: 'rtl' }}>
-            {preview}
+      {context.dict && context.dict.map((item, index) => (
+        <div key={index} className="mt-6 p-4 w-full max-w-4xl bg-white rounded-lg shadow-lg">
+          <h3 className="text-xl mb-2">پیش‌ نمایش پیام:</h3>
+          <p className="text-gray-700" style={{ whiteSpace: "pre-wrap", direction: "rtl" }}>
+            {item.result}
           </p>
         </div>
-      )}
+      ))}
     </div>
   );
 };
--- src/page/subPage/desk/marketing/createlist.js
+++ src/page/subPage/desk/marketing/createlist.js
@@ -7,12 +7,44 @@ import { TabulatorFull as Tabulator } from "tabulator-tables";
 import { MdOutlineCreateNewFolder } from "react-icons/md";
 import ModalFilter from "../../../../componet/modalFilter";
 import { AccessContext } from "../../../../config/accessContext";
+import axios from "axios";
+import { OnRun } from "../../../../config/config";
+import Smspage from "../../../../componet/smspage";
 
 const CreateList = () => {
   const access = useContext(AccessContext);
-  console.log(access);
+  const [listConfig, setListConfig] = useState([])
+  const [Config, setConfig] = useState(null)
   const [df, setDf] = useState(null);
+  const [columns, setcolumns] = useState([]);
   const [isOpenFilter, setIsOpenFilter] = useState(false);
+  const [isOpenSender, setIsOpenSender] = useState(false);
+
+  const getConfigList = () =>{
+    axios({method:"POST", url:OnRun+'/marketing/marketinglist',data:{access:access}})
+    .then(response=>{
+
+      setListConfig(response.data);
+      setConfig(response.data[0])
+    })
+  }
+
+
+  const getDf = () =>{
+
+    if(Config){
+      axios({method:"POST", url:OnRun+'/marketing/columnmarketing',data:{access:access,_id:Config}})
+      .then(response=>{
+        console.log(response.data)
+        setDf(response.data.df)
+        setcolumns(response.data.columns)
+      })
+
+    }
+  }
+
+
+
 
   useEffect(() => {
     if (df) {
@@ -38,9 +70,14 @@ const CreateList = () => {
       };
     }
   }, [df]);
+  useEffect(getConfigList,[])
+  useEffect(getDf,[Config])
 
   const toggleModal = () => {
     setIsOpenFilter(!isOpenFilter);
+  };
+  const toggleModalSender = () => {
+    setIsOpenSender(!isOpenSender);
   };
 
   return (
@@ -62,16 +99,31 @@ const CreateList = () => {
           <span>خروجی CSV</span>
         </p>
         <div className="btntls">
+          <button className="inp-fld" onClick={toggleModalSender}>
+            ارسال
+            <MdOutlineCreateNewFolder className="mt-1" />
+          </button>
           <button className="inp-fld" onClick={toggleModal}>
             ایجاد
             <MdOutlineCreateNewFolder className="mt-1" />
           </button>
+          <select
+            value={Config}
+            onChange={(e) => setConfig(e.target.value)}
+          >
+            {listConfig.map((i) => (
+              <option key={i._id} value={i._id}>
+                {i.title}
+              </option>
+            ))}
+          </select>
         </div>
       </div>
 
       {/* {df === null ? <MiniLoader /> : df === false ? <NoData /> : null} */}
 
       <div>{isOpenFilter && <ModalFilter toggleModal={toggleModal} access={access}/>}</div>
+      <div>{isOpenSender && <Smspage toggleModal={toggleModalSender} Config={Config} columns={columns} access={access}/>}</div>
       <div id="data-table"></div>
     </div>
   );
