c7c16617e5ba41e1bf2cd7d0a13a4c613bb93ceb
zahraAmini <zaharaamini.yazd20@gmail.com>
2024-08-01 10:05:07 +0330

    ..

--- src/page/companyCity.jsx
+++ src/page/companyCity.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
 import { Autocomplete, Button, Chip, Stack, TextField } from "@mui/material";
 import axios from "axios";
