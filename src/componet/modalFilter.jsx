a33476d88f9acd9c6ab1f01481c74a61526f0d25
mohammadali <mohammadalisadat.20@gmail.com>
2024-08-04 13:50:38 +0330

    .

--- src/componet/modalFilter.jsx
+++ src/componet/modalFilter.jsx
@@ -10,7 +10,7 @@ import axios from "axios";
 import { OnRun } from "../config/config";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { Button, Step, StepLabel, Stepper } from "@mui/material";
+import { Button, Step, StepLabel, Stepper, TextField } from "@mui/material";
 import CardConfigMarketing from "./CardConfigMarketing"
 import { FormControl, InputLabel, Select, MenuItem } from "@mui/material";
 import DatePicker from "react-multi-date-picker";
@@ -94,11 +94,12 @@ const ModalFilter = ({ toggleModal, access }) => {
         });
     } else {
       setConfig({
-        title: null,
+        title: "",
         send_time: null,
         period: null,
         context: "",
-        period: "ones",
+        // eslint-disable-next-line no-dupe-keys
+        period: "",
         nobours: {
           enabled: true,
           name: null,
@@ -159,15 +160,17 @@ const ModalFilter = ({ toggleModal, access }) => {
     </>
   );
   const sendingOptions = () => {
+
     return (
       <div className="max-w-lg mx-auto p-8 bg-white rounded-xl shadow-xl">
         {/* عنوان */}
         <FormControl fullWidth className="mt-4">
           <TextField
             id="outlined-basic"
+            
             label={config.title}
             variant="outlined"
-            disabled
+            
           />
         </FormControl>
 
